290753bd08bdb50d403d22795bc55dc2e62ba068
PrernaSharma01 <93273117+PrernaSharma01@users.noreply.github.com>
2025-04-22 08:22:16 +0530

    Basic Test Case in Dynamic Array

--- data_structures/DynamicArray/DynamicArray.java
+++ data_structures/DynamicArray/DynamicArray.java
@@ -1,4 +1,5 @@
 package DynamicArray;
+import java.util.*;
 
 public class DynamicArray {
     private int[] dArray;
@@ -38,5 +39,63 @@ public class DynamicArray {
     public void removeLast(){
         dArray[size] = -1;
         size--;
+    }
+
+    private boolean verifyEachElementInLists(DynamicArray arr, List<Integer> list){
+        if (arr.getSize() != list.size()) return false;
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) != arr.get(i)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean basicTestCase(DynamicArray dArr){
+        boolean isValid = true;
+        List<Integer> testList = new ArrayList<>();
+
+        dArr.add(50);
+        testList.add(50);
+        dArr.add(25);
+        testList.add(25);
+        dArr.add(15);
+        testList.add(15);
+        isValid = verifyEachElementInLists(dArr,testList);
+        if (!isValid) return isValid;
+        
+        dArr.set(0,75);
+        testList.set(0,75);
+        dArr.set(1,50);
+        testList.set(1,50);
+        isValid = verifyEachElementInLists(dArr,testList);
+        if (!isValid) return isValid;
+      
+        return isValid;
+    }
+
+    private boolean robustTestCase(){
+        boolean isValid = true;
+        // To Implement
+        
+        return isValid;
+    }
+
+    private boolean randomInterleaveTestCase(){
+        boolean isValid = true;
+        // To Implement
+
+        return isValid;
+    }
+
+    public static void main (String args[]){
+        boolean isTestSuccess = true;
+        DynamicArray dArr = new DynamicArray();
+        long startTime = System.currentTimeMillis(); 
+        isTestSuccess = dArr.basicTestCase(dArr);
+        long endTime = System.currentTimeMillis();  
+        System.out.println("Basic Test Case - Time taken: " + (endTime - startTime) + " ms");
+        System.out.println("Basic Test Case - Result "+isTestSuccess);
+
     }
 }
